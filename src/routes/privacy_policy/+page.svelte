d34a6182a2d4c6a53154cdcff38e07f04ca4f865
siddharth <me@siddahrthjain.in>
2023-10-19 22:27:40 +0530

    Privacy and Terms page add4d

---
+++ src/routes/privacy_policy/+page.svelte
@@ -0,0 +1,35 @@
+<div class="flex flex-col gap-4 px-3 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+
+
+    <h1 class="text-2xl font-bold">Privacy Policy</h1>
+
+    <h2 class="text-lg font-bold">Collection of Routine Information</h2>
+
+    <p>This website track basic information about their visitors. This information includes, but is not limited to, IP addresses, browser details, timestamps and referring pages. None of this information can personally identify specific visitors to this website. The information is tracked for routine administration and maintenance purposes.</p>
+
+    <h2 class="text-lg font-bold">Cookies</h2>
+
+    <p>Where necessary, this website uses cookies to store information about a visitor’s preferences and history in order to better serve the visitor and/or present the visitor with customized content.</p>
+
+    <h2 class="text-lg font-bold">Advertisement and Other Third Parties</h2>
+
+    <p>Advertising partners and other third parties may use cookies, scripts and/or web beacons to track visitor activities on this website in order to display advertisements and other useful information. Such tracking is done directly by the third parties through their own servers and is subject to their own privacy policies. This website has no access or control over these cookies, scripts and/or web beacons that may be used by third parties. Learn how to <a href="http://www.google.com/privacy_ads.html">opt out of Google’s cookie usage</a>.</p>
+
+    <h2 class="text-lg font-bold">Links to Third Party Websites</h2>
+
+    <p>We have included links on this website for your use and reference. We are not responsible for the privacy policies on these websites. You should be aware that the privacy policies of these websites may differ from our own.</p>
+
+    <h2 class="text-lg font-bold">Security</h2>
+
+    <p>The security of your personal information is important to us, but remember that no method of transmission over the Internet, or method of electronic storage, is 100% secure. While we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security.</p>
+
+    <h2 class="text-lg font-bold">Changes To This Privacy Policy</h2>
+
+    <p>This Privacy Policy is effective as of [[Date]] and will remain in effect except with respect to any changes in its provisions in the future, which will be in effect immediately after being posted on this page.</p>
+
+    <p>We reserve the right to update or change our Privacy Policy at any time and you should check this Privacy Policy periodically. If we make any material changes to this Privacy Policy, we will notify you either through the email address you have provided us, or by placing a prominent notice on our website.</p>
+
+    <h2 class="text-lg font-bold">Contact Information</h2>
+
+    <p>For any questions or concerns regarding the privacy policy, please send us an email to contact@utilitygods.com.</p>
+</div>
---
+++ src/routes/terms_of_use/+page.svelte
@@ -0,0 +1,68 @@
+<div class="flex flex-col gap-4 px-3 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+
+    <h1 class="text-2xl font-bold">TERMS OF SERVICE</h1>
+
+    <p>Last Updated: 6 Oct. 2023</p>
+
+    <p>These terms of service ("Terms") apply to your access and use of https://www.captionitnow.com (the "Service"). Please read them carefully.</p>
+
+    <h2 class="text-lg font-bold">Accepting these Terms</h2>
+
+    <p>If you access or use the Service, it means you agree to be bound by all of the terms below. So, before you use the Service, please read all of the terms. If you don't agree to all of the terms below, please do not use the Service. Also, if a term does not make sense to you, please let us know by e-mailing [SUPPORT EMAIL ADDRESS].</p>
+
+    <h2 class="text-lg font-bold">Changes to these Terms</h2>
+
+    <p>We reserve the right to modify these Terms at any time. For instance, we may need to change these Terms if we come out with a new feature or for some other reason.</p>
+
+    <p>Whenever we make changes to these Terms, the changes are effective [TIME PERIOD] after we post such revised Terms (indicated by revising the date at the top of these Terms) or upon your acceptance if we provide a mechanism for your immediate acceptance of the revised Terms (such as a click-through confirmation or acceptance button). It is your responsibility to check [APP NAME] for changes to these Terms.</p>
+
+    <p>If you continue to use the Service after the revised Terms go into effect, then you have accepted the changes to these Terms.</p>
+
+    <h2 class="text-lg font-bold">Privacy Policy</h2>
+
+    <p>For information about how we collect and use information about users of the Service, please check out our privacy policy available at [INSERT LINK].</p>
+
+    <h2 class="text-lg font-bold">Third-Party Services</h2>
+
+    <p>From time to time, we may provide you with links to third party websites or services that we do not own or control. Your use of the Service may also include the use of applications that are developed or owned by a third party. Your use of such third party applications, websites, and services is governed by that party's own terms of service or privacy policies. We encourage you to read the terms and conditions and privacy policy of any third party application, website or service that you visit or use.</p>
+
+    <h2 class="text-lg font-bold">Creating Accounts</h2>
+
+    <p>When you create an account or use another service to log in to the Service, you agree to maintain the security of your password and accept all risks of unauthorized access to any data or other information you provide to the Service.</p>
+
+    <p>If you discover or suspect any Service security breaches, please let us know as soon as possible.</p>
+
+    <h2 class="text-lg font-bold">Your Content & Conduct</h2>
+
+    <p>[CUSTOMIZE THE FOLLOWING SECTION BASED ON THE TYPE OF CONTENT SUBMITTED BY USERS.]</p>
+
+    <p>Our Service allows you and other users to post, link and otherwise make available content. You are responsible for the content that you make available to the Service, including its legality, reliability, and appropriateness.</p>
+
+    <p>When you post, link or otherwise make available content to the Service, you grant us the right and license to use, reproduce, modify, publicly perform, publicly display and distribute your content on or through the Service. We may format your content for display throughout the Service, but we will not edit or revise the substance of your content itself.</p>
+
+    <p>Aside from our limited right to your content, you retain all of your rights to the content you post, link and otherwise make available on or through the Service.</p>
+
+    <p>[CUSTOMIZE THE FOLLOWING SECTION BASED ON YOUR DATA RETENTION PRACTICES.]</p>
+
+    <p>You can remove the content that you posted by deleting it. Once you delete your content, it will not appear on the Service, but copies of your deleted content may remain in our system or backups for some period of time. We will retain web server access logs for a maximum of [TIME PERIOD] and then delete them.</p>
+
+    <p>You may not post, link and otherwise make available on or through the Service any of the following:</p>
+
+    <p>[INCLUDE ANY OTHER CONTENT RESTRICTIONS THAT MAY BE UNIQUE TO YOUR COMPANY.]</p>
+
+    <ul>
+        <li class="ml-4 list-disc">Content that is libelous, defamatory, bigoted, fraudulent or deceptive;</li>
+        <li class="ml-4 list-disc">Content that is illegal or unlawful, that would otherwise create liability;</li>
+        <li class="ml-4 list-disc">Content that may infringe or violate any patent, trademark, trade secret, copyright, right of privacy, right of publicity or other intellectual or other right of any party;</li>
+        <li class="ml-4 list-disc">Mass or repeated promotions, political campaigning or commercial messages directed at users who do not follow you (SPAM);</li>
+        <li class="ml-4 list-disc">Private information of any third party (e.g., addresses, phone numbers, email addresses, Social Security numbers and credit card numbers); and</li>
+        <li class="ml-4 list-disc">Viruses, corrupted data or other harmful, disruptive or destructive files or code.</li>
+        
+        Also, you agree that you will not do any of the following in connection with the Service or other users:
+        
+        <li class="ml-4 list-disc">Use the Service in any manner that could interfere with, disrupt, negatively affect or inhibit other users from fully enjoying the Service or that could damage, disable, overburden or impair the functioning of the Service;</li>
+        <li class="ml-4 list-disc">Impersonate or post on behalf of any person or entity or otherwise misrepresent your affiliation with a person or entity;</li>
+        <li class="ml-4 list-disc">Collect any personal information about other users, or intimidate, threaten, stalk or otherwise harass other users of the Service;</li>
+        <li class="ml-4 list-disc">Create an account or post any content if you are not over 13 years of age years of age; and</li>
+        <li class="ml-4 list-disc">Circumvent or attempt to circumvent any filtering, security measures, rate limits or other features designed to protect the Service, users of the Service, or third parties.</li>
+</div>
